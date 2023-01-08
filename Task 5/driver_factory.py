f3328a10cee32068d2dea654645863fbcf5875ec
benjamin_b1 <benjamin.bonnaire@cogeco.com>
2023-01-08 12:50:05 -0500

    added support for windows

---
+++ Task 5/driver_factory.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+# -*- coding: UTF-8 -*-
+from selenium.common import exceptions as exceptions
+import json
+
+
+
+
+class DriverFactory():
+    """
+    This driver factory class contains the used selenium methods for the purpose of this exercise
+
+    """
+    def __init__(self):
+        f = open("./config.json",)
+        config = json.load(f)
+        driver_path = config[config["os"]]
+        self.driver = webdriver.Chrome(executable_path=driver_path)
+        
+    
+    def get_all_elements_by_xpath(self,xpath):
+        """
+        Function to retreive all the elements corresponding to an xpath
+
+        Args:
+            xpath (String): HTML relative or Absolute path to all the requested elements
+
+        Returns:
+            List <WebElements>: The list of all webElements found
+        """
+        elements = self.driver.find_elements("xpath",xpath)
+        return elements
+
+    def open_url(self,url):
+        """
+        Function to go to specified URL
+
+        Args:
+            url (String): Destination URL
+        """
+        self.driver.get(url)
+
+    def close(self):
+        """
+        Function to close Browser
+
+        """
+        self.driver.close()
